8b7012a7d2f7bf3b01c852bb47b69db0f4207dd1
shihaoche <108776672+shihaoche@users.noreply.github.com>
2022-10-07 08:43:36 +0800

    Update map.js

--- map.js
+++ map.js
@@ -1,26 +1,17 @@
-// 地圖基本設定
 function initMap() {
 
-  // 基本地圖設定
-  const uluru = { lat: 25.0501101, lng: 121.5255337  }; // 給一個初始位置 台北車站的經緯度
-  // 地圖設定
+  const uluru = { lat: 25.0501101, lng: 121.5255337  }; 
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12, //放大的倍率
-    center: uluru, //初始化的地圖中心位置
-       // maxZoom: 20, // 使用者能縮放地圖的最大比例 
-        // minZoom: 3, // 使用者能縮放地圖的最小比例
-        // gestureHandling: "auto", //手勢
-        streetViewControl: false, // 是否顯示右下角街景小人
-        mapTypeControl: false // 使用者能否切換地圖樣式：一般、衛星圖等
+    zoom: 12, 
+    center: uluru,
+        streetViewControl: false, 
+        mapTypeControl: false
   });
-
-  // 標示設定
   const marker = new google.maps.Marker({
-    position: uluru, //marker的放置位置
-    map: map //這邊的map指的是map變數
+    position: uluru, 
+    map: map 
   });
 
-//Google MAP 進入時自動定位
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
@@ -35,19 +26,9 @@ if (navigator.geolocation) {
         map.setCenter(pos);
     });
 } else {
-    // Browser doesn't support Geolocation
     alert("未允許或遭遇錯誤！");
 }
 
-
-
-
-  
-
-
-
-
-// 搜尋設定
   const input = document.getElementById('pac-input');
   const searchBox = new google.maps.places.SearchBox(input);
 
@@ -55,7 +36,6 @@ if (navigator.geolocation) {
       searchBox.setBounds(map.getBounds());
   });
 
-  // 搜尋後定位
   searchBox.addListener("places_changed", () => {
     const places = searchBox.getPlaces();
   
@@ -64,13 +44,11 @@ if (navigator.geolocation) {
     }
 
     let markers = [];
-    // 清除舊標記。
     markers.forEach((marker) => {
       marker.setMap(null);
     });
     markers = [];
   
-    // 對於每個地點，獲取圖標、名稱和位置。
     const bounds = new google.maps.LatLngBounds();
   
     places.forEach((place) => {
@@ -78,18 +56,7 @@ if (navigator.geolocation) {
         console.log("Returned place contains no geometry");
         return;
       }
-  
 
-      // const icon = {  //設定ICON標示圖
-      //   url: place.icon,
-      //   size: new google.maps.Size(71, 71),
-      //   origin: new google.maps.Point(0, 0),
-      //   anchor: new google.maps.Point(17, 34),
-      //   scaledSize: new google.maps.Size(25, 25),
-      // };
-
-  
-      // Create a marker for each place.
       markers.push(
         new google.maps.Marker({
           map,
